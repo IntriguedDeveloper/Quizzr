4115d1a98db8648026c8e23e467f10da768205c9
Ankit Raj <ruby08241@gmail.com>
2025-01-10 18:38:14 +0530

    style: format code for improved readability and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,42 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "nextjs-toploader/app";
 import styles from "./page.module.css";
 import { useUserContext } from "./context/UserContext";
 import LoadingSpinner from "./LoadingSpinner";
 
 const Home: React.FC = () => {
-  const router = useRouter();
-  const userData = useUserContext();
+	const router = useRouter();
+	const userData = useUserContext();
 
-  useEffect(() => {
-    const getUserData = async () => {
-      if (!userData.isLoading) {
-        console.log("User Data:", userData);
-        
-        if (userData.userEmail) {
-          console.log("This is user data: " + userData.userEmail);
+	useEffect(() => {
+		const getUserData = async () => {
+			if (!userData.isLoading) {
+				console.log("User Data:", userData);
 
-          if (userData.isAdmin) {
-            router.push(`/admin/home`);
-          } else {
-            router.push("/home");
-          }
-        } else {
-          router.push("/auth");
-        }
-      }
-    };
+				if (userData.userEmail) {
+					console.log("This is user data: " + userData.userEmail);
 
-    getUserData();
-  }, [userData, router]);
+					if (userData.isAdmin) {
+						router.push(`/admin/home`);
+					} else {
+						router.push("/home");
+					}
+				} else {
+					router.push("/auth");
+				}
+			}
+		};
 
-  return (
-    <div className={styles.container}>
-      <LoadingSpinner></LoadingSpinner>
-    </div>
-  );
+		getUserData();
+	}, [userData, router]);
+
+	return (
+		<div className={styles.container}>
+			<LoadingSpinner></LoadingSpinner>
+		</div>
+	);
 };
 
 export default Home;
